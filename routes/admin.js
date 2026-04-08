e4662827bbde8352303cc3de6d373f738be402d5
Hadi <haditrumboo@gmail.com>
2026-04-08 12:59:37 +0530

    feat: add edit course route with ownership validation

--- routes/admin.js
+++ routes/admin.js
@@ -45,7 +45,7 @@ adminRouter.post('/login', async (req, res) => {
             return res.status(400).json({ success: false, message: "invalid email or password" })
         }
 
-        const token = Jwt.sign({ adminId: user._id }, process.env.JWT_ADMIN_SECRET1, { expiresIn: "7d"});
+        const token = Jwt.sign({ adminId: user._id }, process.env.JWT_ADMIN_SECRET1, { expiresIn: "7d" });
         res.cookie("token", token, {
             httpOnly: true,
             sameSite: 'strict',                   // CSRF protection
@@ -61,23 +61,54 @@ adminRouter.post('/login', async (req, res) => {
 
 adminRouter.post("/create-course", adminmiddleware, async (req, res) => {
     try {
-   const adminId = req.adminId;
-    const { title, description, price, imageUrl, creatorId } = req.body;
+        const adminId = req.adminId;
+        const { title, description, price, imageUrl, creatorId } = req.body;
 
-    const course = await courseModel.create({
-        title, 
-        description,
-        price,
-        imageUrl,
-        creatorId: adminId
-    })
-res.status(201).json({ success: true, message: 'course created successfully',
-    courseId: course._id
-})
+        const course = await courseModel.create({
+            title,
+            description,
+            price,
+            imageUrl,
+            creatorId: adminId
+        })
+        res.status(201).json({
+            success: true, message: 'course created successfully',
+            courseId: course._id
+        })
     }
     catch (error) {
         res.status(500).json({ success: false, message: 'error creating course' })
     }
 
+})
+adminRouter.put("/edit-course/:courseId", adminmiddleware, async (req, res) => {
+    try {
+        const courseId = req.params.courseId
+        const adminId = req.adminId;
+        const { title, description, price, } = req.body;
+
+        if (!title || !description || !price) {
+            return res.status(400).json({ success: false, message: "all fields are required" })
+        }
+        const updatedCourse = await courseModel.findOneAndUpdate(
+            { _id: courseId, creatorId: adminId },
+            { title, description, price },
+             { returnDocument: 'after' }
+        );
+
+        if (!updatedCourse) {
+            return res.status(404).json({
+                success: false,
+                message: "Course not found or unauthorized"
+            });
+        }
+        res.status(200).json({
+            success: true, message: 'course updated successfully',
+            courseId: updatedCourse._id
+        })
+    }
+    catch (error) {
+        res.status(500).json({ success: false, message: 'error updating course', error: error.message})
+    }
 })
 module.exports = { adminRouter }
